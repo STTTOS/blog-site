83db0f07f186624994ae95439746016e1ef2bb00
xuan <744765302@qq.com>
2024-04-11 11:38:32 +0800

    formatter: 修复lint问题

--- src/service/storage/index.ts
+++ src/service/storage/index.ts
@@ -1,4 +1,5 @@
 import type { Key } from 'react'
+import type { StroageItemProps } from '@/page/manage/storage/Permanet/staticModel'
 
 import { message } from 'antd'
 
@@ -7,12 +8,12 @@ import request from '../../utils/http'
 async function getPersistentFileList() {
   const {
     data: { list }
-  } = await request<{ list: string[] }>('api/storage/persistent/list')
+  } = await request<{ list: StroageItemProps[] }>('api/storage/persistent/list')
   return { list, total: 0 }
 }
 
 async function deleteFile(params: { id: Key }) {
-  await request<{ list: string[] }>('api/storage/persistent/delete', params)
+  await request('api/storage/persistent/delete', params)
   message.success('删除成功')
 }
 
