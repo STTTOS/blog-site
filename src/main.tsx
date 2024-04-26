80994b7cc00df8a2d50a8aac8887ad3cd9a77c3b
xuan <744765302@qq.com>
2024-04-26 11:29:50 +0800

    feat: 更新react版本

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import 'lazysizes'
 import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
-import { render } from 'react-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import { useRoutes, BrowserRouter } from 'react-router-dom'
 
@@ -18,12 +19,12 @@ const App = () => {
   useTimeout(() => countWeb(), 5 * 1000, process.env.NODE_ENV === 'development')
 
   return (
-    // <React.StrictMode>
-    <BrowserRouter>
-      <Index />
-    </BrowserRouter>
-    // </React.StrictMode>
+    <StrictMode>
+      <BrowserRouter>
+        <Index />
+      </BrowserRouter>
+    </StrictMode>
   )
 }
 
-render(<App />, document.getElementById('blog-app')!)
+createRoot(document.getElementById('blog-app')!).render(<App />)
