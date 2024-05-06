b3bc8fa2f9f3abfce425f97d7e0289cb7cba94f9
xuan <744765302@qq.com>
2024-05-06 18:35:42 +0800

    fix: 调整403页面交互

--- src/page/403/index.tsx
+++ src/page/403/index.tsx
@@ -1,23 +1,28 @@
 import { Space, Button } from 'antd'
+import { useNavigate } from 'react-router'
 import { ExclamationCircleOutlined } from '@ant-design/icons'
 
 import styles from './index.module.less'
 
 const Forbidden = () => {
+  const nav = useNavigate()
   return (
     <div className={styles.wrapper}>
       <div className={styles.top}>
         <ExclamationCircleOutlined className={styles.icon} />
         <Space direction="vertical">
           <div className={styles.title}>哦豁</div>
-          <div className={styles.desc}>莫得权限哈</div>
+          <div className={styles.desc}>莫得权限哈(需要管理员权限)</div>
         </Space>
       </div>
       <Space className={styles.footer}>
-        <Button type="link" href="/">
+        <Button type="link" onClick={() => nav('/')}>
           回到首页
         </Button>
-        <Button type="link" href="/login">
+        <Button type="link" onClick={() => nav(-1)}>
+          返回上一页
+        </Button>
+        <Button type="link" onClick={() => nav('/login')}>
           登录其他账号
         </Button>
       </Space>
