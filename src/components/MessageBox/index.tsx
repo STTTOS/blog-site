7dd0027f33f4e0a6600d398a3d47f71b454cf950
yangchengrong <yangchnegrong@yzw.cn>
2024-06-12 14:35:54 +0800

    feat: 消息列表下拉动态加载

--- src/components/MessageBox/index.tsx
+++ src/components/MessageBox/index.tsx
@@ -1,9 +1,12 @@
+import dayjs from 'dayjs'
 import { useRequest } from 'ahooks'
-import { useMemo, type FC } from 'react'
 import { MessageOutlined } from '@ant-design/icons'
-import { List, Badge, Empty, Avatar, Popover } from 'antd'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { useMemo, type FC, useState, useEffect } from 'react'
+import { List, Badge, Empty, Avatar, Button, Popover } from 'antd'
 
 import request from '@/utils/http'
+import styles from './index.module.less'
 import { User } from '@/service/user/types'
 
 type MessageType = 'reply' | 'like' | 'comment'
@@ -14,72 +17,159 @@ interface Message {
   sender?: User
   type: MessageType
   articleId?: number
+  isRead: boolean
 }
 interface MessageBoxProps {
-  // total: number
-  // list: Message[]
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any
 }
+
+const readableDateStr = (time: string) => {
+  const date = dayjs(time)
+  const currentDate = dayjs()
+
+  const days = currentDate.diff(date, 'days')
+  if (days > 3) {
+    // 如果不是当年的, 则展示年份
+    if (currentDate.get('year') !== date.get('year')) {
+      return date.format('YYYY-MM-DD')
+    }
+    // 如果超过3天, 则展示月份/天
+    return date.format('MM-DD')
+  }
+
+  // 如果是3天内, 直接调用fromNow
+  return date.fromNow()
+}
+const pageSize = 10
 // 消息盒子
 const MessageBox: FC<MessageBoxProps> = () => {
-  const { data, mutate } = useRequest(() =>
-    request<{ list: Message[]; total: number }>('api/message/unread').then(
-      (res) => res.data
+  const [list, setList] = useState<Message[]>([])
+  const [current, setCurrent] = useState(0)
+  const [total, setTotal] = useState(0)
+  const { runAsync, loading } = useRequest(
+    (page = 1) =>
+      request<{ list: Message[]; total: number }>('api/message/list', {
+        pageSize,
+        current: page
+      }).then((res) => res.data),
+    { manual: true }
+  )
+  const { data, refreshAsync: refreshCount } = useRequest(() =>
+    request<{ count: number }>('api/message/unreadCount').then(
+      (props) => props.data
     )
   )
-  const { total = 0, list = [] } = data || {}
+  const { runAsync: readAll, loading: reading } = useRequest(
+    () => request<{ count: number }>('api/message/readAll'),
+    { manual: true }
+  )
 
+  const loadMore = async () => {
+    if (loading) return
+
+    const res = await runAsync(current + 1)
+    setList(list.concat(res.list))
+    setTotal(res.total)
+    setCurrent((pre) => pre + 1)
+  }
   const renderType = (type: MessageType) => {
     if (type === 'reply') return '回复了我的评论'
     else if (type === 'comment') return '评论了我的文章'
   }
   const listContent = useMemo(() => {
-    if (list.length === 0) return <Empty />
+    return list.map(
+      ({ id, createdAt, content, sender, articleId, type, isRead }) => {
+        return (
+          <List.Item
+            key={id}
+            style={{ cursor: 'pointer', gap: '20px', position: 'relative' }}
+            onClick={async (e) => {
+              e.stopPropagation()
 
-    return list.map(({ id, createdAt, content, sender, articleId, type }) => {
-      return (
-        <List.Item
-          key={id}
-          style={{ cursor: 'pointer', gap: '20px' }}
-          onClick={async (e) => {
-            e.stopPropagation()
-
-            request('api/message/read', { id }).then(() => {
-              mutate({
-                list: list.filter((item) => item.id !== id),
-                total: Math.max(total - 1, 0)
+              await request('api/message/read', { id }).then(() => {
+                setList(
+                  list.map((props) => {
+                    if (props.id === id) {
+                      return { ...props, isRead: true }
+                    }
+                    return props
+                  })
+                )
               })
-            })
-            articleId && window.open(`/article/${articleId}`)
-          }}
-        >
-          <List.Item.Meta
-            avatar={<Avatar src={sender?.avatar} />}
-            title={
-              <div style={{ display: 'flex' }}>
-                <em style={{ whiteSpace: 'nowrap' }}>{sender?.name}</em>
-                <span style={{ marginLeft: 4, fontWeight: 'normal' }}>
-                  {renderType(type)}
-                </span>
-              </div>
-            }
-            description={<em>{content}</em>}
-          />
-          <div>{createdAt}</div>
-        </List.Item>
-      )
-    })
+              refreshCount()
+              articleId && window.open(`/article/${articleId}`)
+            }}
+          >
+            <List.Item.Meta
+              avatar={<Avatar src={sender?.avatar} />}
+              title={
+                <div style={{ display: 'flex' }}>
+                  <em style={{ whiteSpace: 'nowrap' }}>{sender?.name}</em>
+                  <span style={{ marginLeft: 4, fontWeight: 'normal' }}>
+                    {renderType(type)}
+                  </span>
+                </div>
+              }
+              description={<em>{content}</em>}
+            />
+            <div>{readableDateStr(createdAt)}</div>
+            {!isRead && <div className={styles.unRead}></div>}
+          </List.Item>
+        )
+      }
+    )
   }, [list])
+
+  const handleReadAll = async () => {
+    await readAll()
+    await refreshCount()
+    setList(list.map((props) => ({ ...props, isRead: true })))
+  }
+  const hasMore = useMemo(() => {
+    return list.length < total
+  }, [list, total])
+
+  const showReadAll = useMemo(() => {
+    return list.filter((props) => !props.isRead).length > 0
+  }, [list])
+
+  useEffect(() => {
+    loadMore()
+  }, [])
   return (
     <Popover
       content={
-        <List header={<span>消息中心</span>} style={{ width: 600 }}>
-          {listContent}
+        <List
+          header={
+            <div className={styles.header}>
+              <span>消息中心</span>
+              {showReadAll && (
+                <Button type="link" onClick={handleReadAll} loading={reading}>
+                  全部标记已读
+                </Button>
+              )}
+            </div>
+          }
+          style={{ width: 600 }}
+          id="s"
+        >
+          {list.length === 0 ? (
+            <Empty />
+          ) : (
+            <InfiniteScroll
+              height={600}
+              next={loadMore}
+              hasMore={hasMore}
+              dataLength={list.length}
+              loader={<div className={styles.loading}>Loading ...</div>}
+            >
+              {listContent}
+            </InfiniteScroll>
+          )}
         </List>
       }
     >
-      <Badge count={total}>
+      <Badge count={data?.count || 0}>
         <MessageOutlined style={{ fontSize: 20, fontWeight: 'bold' }} />
       </Badge>
     </Popover>
