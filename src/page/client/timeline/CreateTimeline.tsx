2b2f7e4d777721a6001c56bd477c872992128d26
yangchengrong <yangchnegrong@yzw.cn>
2025-02-18 14:33:03 +0800

    feat: 创建时间轴时增加`order`字段

--- src/page/client/timeline/CreateTimeline.tsx
+++ src/page/client/timeline/CreateTimeline.tsx
@@ -1,8 +1,8 @@
 import Box from '@mui/joy/Box'
 import Input from '@mui/joy/Input'
-import { Form, Drawer } from 'antd'
 import Textarea from '@mui/joy/Textarea'
 import { FC, ComponentType } from 'react'
+import { Form, Space, Drawer, Switch } from 'antd'
 
 import Upload from '@/components/Upload'
 import { upload } from '@/service/common'
@@ -40,7 +40,11 @@ const AntdFormTextarea = withAdaptAntdFormItem(Textarea)
 const CreateTimeline: FC<CreateTimelineProps> = ({ onCreate }) => {
   const [form] = Form.useForm()
   const handleCreate = async () => {
-    await onCreate(await form.validateFields())
+    const { order, ...rest } = await form.validateFields()
+    await onCreate({
+      ...rest,
+      order: order ? 'desc' : 'asc'
+    })
   }
   return (
     <Drawer
@@ -63,7 +67,8 @@ const CreateTimeline: FC<CreateTimelineProps> = ({ onCreate }) => {
     >
       <Form
         initialValues={{
-          cover: defaultTimelineCover
+          cover: defaultTimelineCover,
+          order: true
         }}
         form={form}
         style={{ display: 'flex', justifyContent: 'center' }}
@@ -78,16 +83,24 @@ const CreateTimeline: FC<CreateTimelineProps> = ({ onCreate }) => {
             alignItems: 'center'
           }}
         >
-          <Form.Item
-            name="title"
-            rules={[{ required: true, message: '标题不能为空' }]}
-          >
-            <AntdFormInput
-              placeholder="输入标题"
-              variant="outlined"
-              size="lg"
-            />
-          </Form.Item>
+          <Space align="center" style={{ justifyContent: 'space-between' }}>
+            <Form.Item
+              name="title"
+              rules={[{ required: true, message: '标题不能为空' }]}
+            >
+              <AntdFormInput
+                placeholder="输入标题"
+                variant="outlined"
+                size="lg"
+              />
+            </Form.Item>
+            <Form.Item name="order">
+              <Switch
+                checkedChildren="优先展示最新内容"
+                unCheckedChildren="优先展示最早内容"
+              ></Switch>
+            </Form.Item>
+          </Space>
 
           <Form.Item name="desc">
             <AntdFormTextarea
